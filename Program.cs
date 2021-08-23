46e3d663a21fbea57481e67c7f9f24eae9e763b4
Sazid <sazid.sunny@gmail.com>
2021-08-23 08:33:43 +0530

    Program.cs Updated

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 
 namespace DotnetCore
 {
+    class Employee
+    {
+        public int Id { get; set;}
+        public string Name {get; set;}
+        public double Salary{get; set;}
+
+        public override string ToString()
+        {
+            return $"{Id} - {Name} -{Salary}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World Updated");
+            Employee employee = new Employee{Id = 4001, Name = "Sazid", Salary = 3000000.000};
         }
     }
 }
